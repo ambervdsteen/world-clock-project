6dea6e3455d0ab89e033a88cefc07a815a89bbd9
ambervdsteen <155630578+ambervdsteen@users.noreply.github.com>
2024-05-11 15:41:01 +0200

    added select functionalities

--- javascript/index.js
+++ javascript/index.js
@@ -7,8 +7,6 @@ let londonTime=moment().tz("Europe/London");
 londonDateElement.innerHTML=londonTime.format("Do [of] MMMM YYYY");
 londonTimeElement.innerHTML=londonTime.format("h:mm:ss [<small>]A[</small]");
 
-
-
 let jakartaElement = document.querySelector("#jkr");
 let jakartaDateElement=jakartaElement.querySelector(".date");
 let jakartaTimeElement=jakartaElement.querySelector(".time");
@@ -16,8 +14,29 @@ let jakartaTime=moment().tz("Asia/Jakarta");
 
 jakartaDateElement.innerHTML=jakartaTime.format("Do [of] MMMM YYYY");
 jakartaTimeElement.innerHTML=jakartaTime.format("h:mm:ss [<small>]A[</small]");
+}
 
+function updateCity (event){
+    let timeZoneCity=event.target.value;
+    let cityName=timeZoneCity.replace("_", " ").split("/")[1];
+    let cityTime=moment().tz(timeZoneCity);
+    let citiesElement=document.querySelector("#cities");
+    citiesElement.innerHTML=`
+    <div class="city"> 
+    <div>
+        <h3>${cityName} </h3>
+        <div class="date"> ${cityTime.format("Do [of] MMMM YYYY")}</div>
+        </div>
+        <div class="time"> ${cityTime.format("h:mm")} <small>${cityTime.format("A")}</small> </div>
+    </div>
+    `
 }
 
 updateTime();
 setInterval(updateTime, 1000);
+
+// add event listener to select
+let citySelectElement = document.querySelector("#city");
+citySelectElement.addEventListener("change", updateCity);
+
+// replace (change innerhtml) when city is chosen
